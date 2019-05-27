c394538b5d3510b34e5ec414f36df839b95737ba
Deniz Yaradanakul <30984243+gabalah@users.noreply.github.com>
2019-05-27 17:48:36 +0300

    Update MoveFileReader.java
    MoveFileReader is updated

--- checkers-software-product-line/src/utils/MoveFileReader.java
+++ checkers-software-product-line/src/utils/MoveFileReader.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import core.*;
 import base.Move;
@@ -15,8 +17,8 @@ public class MoveFileReader {
 	private Scanner sc;
 
 	public MoveFileReader() {
-		filename = "chessMat";
-	    file = new File(filename); 
+		filename = "completeGameForTurkish";
+		file = new File(filename); 
 	  	try {
 			sc = new Scanner(file);
 		} catch (FileNotFoundException e) {
@@ -29,19 +31,36 @@ public class MoveFileReader {
 		String line;
 		while (sc.hasNextLine()) {
 			line = sc.nextLine();
-			//System.out.println(line);
+			System.out.println(line);
+			Pattern pattern = Pattern.compile("\\d+");
+			Matcher matcher = pattern.matcher(line);
+			matcher.find();
+			IPlayer player = referee.getPlayerbyID(Integer.parseInt(matcher.group()));
+			matcher.find();
+			int sourceX = Integer.parseInt(matcher.group());
+			matcher.find();
+			int sourceY = Integer.parseInt(matcher.group());
+			matcher.find();
+			int destinationX = Integer.parseInt(matcher.group());
+			matcher.find();
+			int destinationY = Integer.parseInt(matcher.group());
+			
+			IMoveCoordinate moveCoordinate = new MoveCoordinate(new Coordinate(sourceX,sourceY),
+					new Coordinate(destinationX,destinationY));
+			AbstractMove move = new Move(player, moveCoordinate);
+			automaticMoveList.add(move);
+			/*
 			IPlayer player = referee.getPlayerbyID(Integer.parseInt(line.substring(0, 1)));
 			IMoveCoordinate moveCoordinate = new MoveCoordinate(
 					new Coordinate(Integer.parseInt(line.substring(3, 4)),
 							Integer.parseInt(line.substring(5, 6))), 
 					new Coordinate(Integer.parseInt(line.substring(10, 11)),
 							Integer.parseInt(line.substring(12, 13))));
-
 			AbstractMove move = new Move(player, moveCoordinate);
 			automaticMoveList.add(move);
+			*/
 		}
 
 		return automaticMoveList;
 	}
-
 }
